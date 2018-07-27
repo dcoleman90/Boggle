d2ee3d58bf34e743ad16b8e5f0001ae9bd909351
Drew Coleman <dcolem12@my.westga.edu>
2018-07-27 01:28:18 -0400

    The submit button successfully takes a word and removes it from the randomly generated tiles It will work as long as the submitted words are valid
    still need
     - to attach and display it to the score
    
     - implement alert for incorrect words

--- src/edu/westga/cs6241/babble/view/BabbleGUI.java
+++ src/edu/westga/cs6241/babble/view/BabbleGUI.java
@@ -1,33 +1,53 @@
 package edu.westga.cs6241.babble.view;
 
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
 
 import edu.westga.cs6241.babble.model.RandomLetter;
 import edu.westga.cs6241.babble.model.Word;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 public class BabbleGUI extends GridPane {
-	private Word word;
+	private String word;
 	private ObservableList<String> selectedWord;
 	private ObservableList<String> listOfLetters;
 	private ListView<String> userSelectedWord;
+	private ListView<String> randomLetters;
+	private Button btnSubmitWord;
+	private Button btnReset;
+	private int score;
 
 	public BabbleGUI() {
 		this.listOfLetters = FXCollections.observableArrayList();
 		this.selectedWord = FXCollections.observableArrayList();
 		this.userSelectedWord = new ListView<String>();
+		this.randomLetters = new ListView<String>();
+		this.word = "";
+		this.score = 0;
+		this.buildGridPane();
+	}
+	
+	public void buildGridPane() {
+		this.buildLabels();
 		this.buildGaps();
 		this.buildRandomLetters();
 		this.buildScoreBox();
 		this.buildButtons();
+	}
+
+	private void buildLabels() {
+		Label tiles = new Label("Tiles");
+		this.add(tiles, 1, 1);
 	}
 
 	private void buildGaps() {
@@ -36,23 +56,19 @@ public class BabbleGUI extends GridPane {
 		this.setPadding(new Insets(5, 5, 5, 5));
 	}
 
-	private void buildRandomLetters() {
-		Label tiles = new Label("Tiles");
-		Label testRemove = new Label("Look What Was Selected please Remove");
-		Label testAdd = new Label("");
-		ListView<String> randomLetters = new ListView<String>();
+	public void buildRandomLetters() {
 		for (int count = this.listOfLetters.size(); count < 7; count++) {
 			this.listOfLetters.add(this.randomLetterHelper());
 		}
-		randomLetters.setItems(this.listOfLetters);
-		randomLetters.setOrientation(Orientation.HORIZONTAL);
-		randomLetters.setMaxHeight(30);
-		randomLetters.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		this.randomLetters.setItems(this.listOfLetters);
+		this.randomLetters.setOrientation(Orientation.HORIZONTAL);
+		this.randomLetters.setMaxHeight(30);
+		this.randomLetters.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent arg0) {
-				testRemove.setText("Looked what you clicked " + randomLetters.getSelectionModel().getSelectedItem());
-				if (randomLetters.getSelectionModel().getSelectedItem() != null) {
+				if (BabbleGUI.this.randomLetters.getSelectionModel().getSelectedItem() != null) {
 					BabbleGUI.this.selectedWord.add(randomLetters.getSelectionModel().getSelectedItem());
+					BabbleGUI.this.word += BabbleGUI.this.randomLetters.getSelectionModel().getSelectedItem();
 				}
 				BabbleGUI.this.listOfLetters.remove(randomLetters.getSelectionModel().getSelectedItem());
 				BabbleGUI.this.userSelectedWord.setItems(BabbleGUI.this.selectedWord);
@@ -60,10 +76,15 @@ public class BabbleGUI extends GridPane {
 		});
 		this.userSelectedWord.setOrientation(Orientation.HORIZONTAL);
 		this.userSelectedWord.setMaxHeight(30);
-		this.add(tiles, 1, 1);
-		this.add(randomLetters, 1, 2);
-		this.add(testRemove, 1, 3);
+
+		this.add(this.randomLetters, 1, 2);
 		this.add(this.userSelectedWord, 1, 4);
+	}
+	
+	public void resetGridPane() {
+		this.word = "";
+		this.selectedWord = FXCollections.observableArrayList();
+		this.getChildren().clear();
 	}
 
 	private String randomLetterHelper() {
@@ -71,15 +92,38 @@ public class BabbleGUI extends GridPane {
 		return a2z.getRandomLetter();
 	}
 
-	public ListView<String> buildWordBox(ObservableList<String> pickedLetters) {
-		ListView<String> wordSelected = new ListView<String>();
-		wordSelected.setItems(pickedLetters);
-		wordSelected.setOrientation(Orientation.HORIZONTAL);
-		wordSelected.setMaxHeight(30);
-		return wordSelected;
-	}
 
 	private void buildButtons() {
+		this.btnSubmitWord = new Button("Submit");
+		this.btnSubmitWord.setOnAction(new SubmitWordListener());
+		this.btnReset = new Button("Reset");
+		HBox buttonBox = new HBox();
+		buttonBox.getStyleClass().add("box-padding");
+		buttonBox.getChildren().addAll(this.btnReset, this.btnSubmitWord);
+		this.add(buttonBox, 1, 5);
+
+	}
+
+	private class SubmitWordListener implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent event) {
+			Word testWord = new Word(BabbleGUI.this.word);
+			try {
+				if (testWord.isAWord()) {
+					BabbleGUI.this.score += testWord.getWordValue();
+				//	BabbleGUI.this.resetLetters();
+					System.out.println("true" + testWord.getWordValue());
+					BabbleGUI.this.selectedWord = FXCollections.observableArrayList();
+					BabbleGUI.this.userSelectedWord.setItems(BabbleGUI.this.selectedWord);
+					BabbleGUI.this.resetGridPane();
+					BabbleGUI.this.buildGridPane();;
+				}
+			} catch (FileNotFoundException fnfe) {
+				System.out.println("<--File not found-->");
+			}
+
+		}
 
 	}
 
